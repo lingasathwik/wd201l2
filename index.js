1938812e1a95c501da7b4c95d06c481513995863
lingasathwik <113545779+lingasathwik@users.noreply.github.com>
2022-09-29 13:33:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const fs = require("fs");
+const args = require('minimist')(process.argv.slice(2));
 
 let homeContent = "";
 let projectContent = "";
-let registrationContent="";
+let registrationContent = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
@@ -19,32 +20,35 @@ fs.readFile("project.html", (err, project) => {
   projectContent = project;
 });
 
-fs.readFile("registration.html",(err,reg) => {
-  if (err){
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
     throw err;
   }
-  registrationContent=reg;
+  registrationContent = registration;
 });
 
-let args=require("minimist")(process.argv.slice(2));
-
-http.createServer((request, response) => {
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
+  .createServer((request, response) => {
     let url = request.url;
     response.writeHeader(200, { "Content-Type": "text/html" });
     switch (url) {
-        case "/project":
-          response.write(projectContent);
-          response.end();
-          break;
-        case "/registration":
-          response.write(registrationContent);
-          response.end();
-          break;
-
-        default:
-          response.write(homeContent);
-          response.end();
-          break;
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
     }
   })
-  .listen(args["port"]);
+  .listen(args.port);
+});
